7fd14033736ed01750112ba589e772ae10610e75
juhyunkil <wngue0331@gmail.com>
2023-11-13 17:48:38 +0900

    collect.go update

--- pkg/collector/collect.go
+++ pkg/collector/collect.go
@@ -2,11 +2,9 @@ package collector
 
 import (
 	"context"
-	"fmt"
 	"gpu-metric-collector/pkg/api/metric"
 	"log"
 	"net"
-	"time"
 
 	"github.com/NVIDIA/go-nvml/pkg/nvml"
 	"google.golang.org/grpc"
@@ -17,34 +15,35 @@ import (
 
 func (m *MetricCollector) RunMetricCollector(ctx context.Context) {
 	go m.StartGRPCServer(ctx)
-	go wait.UntilWithContext(ctx, m.MetricCollectingCycle, time.Duration(m.CollectCycle)*time.Second)
+	go wait.UntilWithContext(ctx, m.MetricCollectingCycle, *m.Interval)
 }
 
 func (m *MetricCollector) MetricCollectingCycle(ctx context.Context) {
-	m.MultiMetric.mutex.Lock()
-	defer m.MultiMetric.mutex.Unlock()
+	m.SafeMultiMetric.mutex.Lock()
+	defer m.SafeMultiMetric.mutex.Unlock()
 
-	node, err := m.HostKubeClient.CoreV1().Nodes().Get(context.TODO(), m.MultiMetric.nodeName, v1.GetOptions{})
+	KETI_LOG_L1("[cycle] metric collect cycle run")
+
+	node, err := m.HostKubeClient.CoreV1().Nodes().Get(context.TODO(), m.SafeMultiMetric.MultiMetric.NodeName, v1.GetOptions{})
 	if err != nil {
 		log.Fatalf("cannot get node metric: %s", err)
 	}
 
-	m.MultiMetric.nodeMetric.memoryFree, _ = node.Status.Allocatable.Memory().AsInt64()
-	m.MultiMetric.nodeMetric.milliCPUFree = node.Status.Allocatable.Cpu().MilliValue()
-	m.MultiMetric.nodeMetric.storageFree, _ = node.Status.Allocatable.Storage().AsInt64()
-	m.MultiMetric.nodeMetric.networkRX = 0 //??
-	m.MultiMetric.nodeMetric.networkTX = 0 //??
+	m.SafeMultiMetric.MultiMetric.NodeMetric.MemoryFree, _ = node.Status.Allocatable.Memory().AsInt64()
+	m.SafeMultiMetric.MultiMetric.NodeMetric.MilliCpuFree = node.Status.Allocatable.Cpu().MilliValue()
+	m.SafeMultiMetric.MultiMetric.NodeMetric.StorageFree, _ = node.Status.Allocatable.Storage().AsInt64()
+	m.SafeMultiMetric.MultiMetric.NodeMetric.NetworkRx = 0 //??
+	m.SafeMultiMetric.MultiMetric.NodeMetric.NetworkTx = 0 //??
 
-	if m.MultiMetric.gpuCount != 0 {
+	if m.SafeMultiMetric.MultiMetric.GpuCount != 0 {
 		defer func() {
 			ret := nvml.Shutdown()
 			if ret != nvml.SUCCESS {
-				//log.Fatalf("Unable to shutdown NVML: %v", ret)
-				fmt.Printf("Unable to shutdown NVML: %v\n", ret)
+				log.Fatalf("Unable to shutdown NVML: %v", ret)
 			}
 		}()
 
-		for uuid, gpuMetric := range m.MultiMetric.gpuMetric {
+		for uuid, gpuMetric := range m.SafeMultiMetric.MultiMetric.GpuMetrics {
 			ret := nvml.Init()
 			if ret != nvml.SUCCESS {
 				log.Fatalf("Unable nvml.Init(): %v", ret)
@@ -56,88 +55,42 @@ func (m *MetricCollector) MetricCollectingCycle(ctx context.Context) {
 			}
 
 			memory, _ := device.GetMemoryInfo()
-			gpuMetric.memoryUsed = int64(memory.Used)
+			gpuMetric.MemoryUsed = int64(memory.Used)
 
 			power, _ := device.GetPowerUsage()
-			gpuMetric.powerUsed = int64(power)
+			gpuMetric.PowerUsed = int64(power)
 
 			picTX, _ := device.GetPcieThroughput(0)
-			gpuMetric.pciTX = int64(picTX)
+			gpuMetric.PciTx = int64(picTX)
 
 			pciRX, _ := device.GetPcieThroughput(1)
-			gpuMetric.pciRX = int64(pciRX)
-
-			gpuMetric.memoryGauge = 0   //??
-			gpuMetric.memoryCounter = 0 //??
+			gpuMetric.PciRx = int64(pciRX)
 
 			temperature, _ := device.GetTemperature(0)
-			gpuMetric.temperature = int64(temperature)
+			gpuMetric.Temperature = int64(temperature)
 
 			utilization, _ := device.GetUtilizationRates()
-			gpuMetric.utilization = int64(utilization.Gpu)
+			gpuMetric.Utilization = int64(utilization.Gpu)
 
 			fanSpeed, _ := device.GetFanSpeed()
-			gpuMetric.fanSpeed = int64(fanSpeed)
+			gpuMetric.FanSpeed = int64(fanSpeed)
 
-			gpuMetric.podCount = 0
+			gpuMetric.PodCount = 0
 			//runningPods +++
 		}
 	}
 
-	m.MultiMetric.DumpMultiMetric() //출력 테스트
+	DumpMultiMetric(m.SafeMultiMetric.MultiMetric)        // DEBUGG LEVEL = 1 일때 출력 수행
+	DumpMultiMetricForTest(m.SafeMultiMetric.MultiMetric) // DEBUGG LEVEL = 3 일때 (정량) 출력 수행
 }
 
 func (m *MetricCollector) GetMultiMetric(context.Context, *metric.Request) (*metric.MultiMetric, error) {
-	m.MultiMetric.mutex.Lock()
-	defer m.MultiMetric.mutex.Unlock()
+	m.SafeMultiMetric.mutex.Lock()
+	defer m.SafeMultiMetric.mutex.Unlock()
 
-	response := &metric.MultiMetric{}
-	response.NodeName = m.MultiMetric.nodeName
+	KETI_LOG_L1("[gRPC] get multi metric called")
 
-	response.NodeMetric = &metric.NodeMetric{}
-	response.NodeMetric.MilliCpuTotal = m.MultiMetric.nodeMetric.milliCPUTotal
-	response.NodeMetric.MilliCpuFree = m.MultiMetric.nodeMetric.milliCPUFree
-	response.NodeMetric.MemoryTotal = m.MultiMetric.nodeMetric.memoryTotal
-	response.NodeMetric.MemoryFree = m.MultiMetric.nodeMetric.memoryFree
-	response.NodeMetric.StorageTotal = m.MultiMetric.nodeMetric.storageTotal
-	response.NodeMetric.StorageFree = m.MultiMetric.nodeMetric.storageFree
-	response.NodeMetric.NetworkRx = m.MultiMetric.nodeMetric.networkRX
-	response.NodeMetric.NetworkTx = m.MultiMetric.nodeMetric.networkTX
-	response.GpuMetrics = make(map[string]*metric.GPUMetric)
-
-	for uuid, gpuMetric := range m.MultiMetric.gpuMetric {
-		response.GpuMetrics[uuid] = &metric.GPUMetric{}
-		response.GpuMetrics[uuid].GpuName = gpuMetric.gpuName
-		response.GpuMetrics[uuid].Architecture = gpuMetric.architecture
-		response.GpuMetrics[uuid].MaxClock = gpuMetric.maxClock
-		response.GpuMetrics[uuid].Cudacore = gpuMetric.cudacore
-		response.GpuMetrics[uuid].Bandwidth = gpuMetric.bandwidth
-		response.GpuMetrics[uuid].MaxOperativeTemp = gpuMetric.maxOperativeTemp
-		response.GpuMetrics[uuid].SlowdownTemp = gpuMetric.slowdownTemp
-		response.GpuMetrics[uuid].ShutdownTemp = gpuMetric.shutdownTemp
-		response.GpuMetrics[uuid].MemoryTotal = gpuMetric.memoryTotal
-		response.GpuMetrics[uuid].MemoryUsed = gpuMetric.memoryUsed
-		response.GpuMetrics[uuid].PowerUsed = gpuMetric.powerUsed
-		response.GpuMetrics[uuid].PciRx = gpuMetric.pciRX
-		response.GpuMetrics[uuid].PciTx = gpuMetric.pciTX
-		response.GpuMetrics[uuid].MemoryGauge = gpuMetric.memoryGauge
-		response.GpuMetrics[uuid].MemoryCounter = gpuMetric.memoryCounter
-		response.GpuMetrics[uuid].Temperature = gpuMetric.temperature
-		response.GpuMetrics[uuid].Utilization = gpuMetric.utilization
-		response.GpuMetrics[uuid].FanSpeed = gpuMetric.fanSpeed
-		response.GpuMetrics[uuid].PodMetrics = make(map[string]*metric.PodMetric)
-
-		for podName, podMetric := range gpuMetric.runningPods {
-			response.GpuMetrics[uuid].PodMetrics[podName] = &metric.PodMetric{}
-			response.GpuMetrics[uuid].PodMetrics[podName].MilliCpuUsed = podMetric.milliCPUUsed
-			response.GpuMetrics[uuid].PodMetrics[podName].MemoryUsed = podMetric.memoryUsed
-			response.GpuMetrics[uuid].PodMetrics[podName].StorageUsed = podMetric.storageUsed
-			response.GpuMetrics[uuid].PodMetrics[podName].NetworkRx = podMetric.networkRX
-			response.GpuMetrics[uuid].PodMetrics[podName].NetworkTx = podMetric.networkTX
-		}
-	}
-
-	return response, nil
+	return m.SafeMultiMetric.MultiMetric, nil
 }
 
 func (m *MetricCollector) StartGRPCServer(ctx context.Context) {
@@ -147,7 +100,7 @@ func (m *MetricCollector) StartGRPCServer(ctx context.Context) {
 	}
 	metricServer := grpc.NewServer()
 	metric.RegisterMetricCollectorServer(metricServer, m)
-	fmt.Println("-----:: Metric Collector Server Running... ::-----")
+	KETI_LOG_L1("[gRPC] metric collector server running...")
 	if err := metricServer.Serve(lis); err != nil {
 		klog.Fatalf("failed to serve: %v", err)
 	}
