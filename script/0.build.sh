b0453f73c665138c9d3eda203d6692776a970f66
juhyunkil <wngue0331@gmail.com>
2023-12-14 17:58:00 +0900

    Building Update

--- script/0.build.sh
+++ script/0.build.sh
@@ -10,7 +10,7 @@ dir=$( pwd )
 
 image_name="keti-gpu-metric-collector"
 registry="ketidevit2"
-version="v1.31"
+version="v1.4"
 
 #binary file
 go build -o $dir/../build/_output/bin/$image_name -mod=vendor $dir/../cmd/main.go
